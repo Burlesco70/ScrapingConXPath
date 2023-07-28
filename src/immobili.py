415bc107842a76c95bebca93a567bda828169434
comdav <fornitori@cominato.it>
2023-07-28 09:26:02 +0200

    Aggiunto le opzioni Garage, magazzino, capannoni e palazzi. Modificato Typer con Annotation

--- src/immobili.py
+++ src/immobili.py
@@ -3,6 +3,7 @@ import lxml.html as html
 import pandas as pd
 from datetime import date
 import typer
+from typing_extensions import Annotated
 from typing import List
 import time
 
@@ -14,7 +15,6 @@ XPATH_NUMERO_ANNUNCI = '//div[@class="in-searchList__title is-listMapLayout"]/te
 XPATH_TITOLI = '//a[@class="in-card__title"]/text()'
 XPATH_LINKS = '//a[@class="in-card__title"]/@href'
 XPATH_PREZZI = '//div[@class="in-realEstateListCard__priceOnTop"]/text()|//li[@class="nd-list__item in-feat__item in-feat__item--main in-realEstateListCard__features--main"]/text() | //li[@class="nd-list__item in-feat__item in-feat__item--main in-realEstateListCard__features--main in-realEstateListCard__features--mainText"]/text() | //div[@class="in-realEstateListCard__features--range"]/text()'
-# Nota: purtroppo le descrizioni non sono sempre presenti
 XPATH_DESCRIZIONI = '//p[@class="in-realEstateListCard__descriptionShort"]/text()|//p[@class="in-realEstateListCard__description"]/text()'
 
 
@@ -104,7 +104,7 @@ def parser_content(url: str):
         raise typer.Exit()
 
 
-def affitto_vendita_callback(value: str) -> str:
+def investimento_callback(value: str) -> str:
     if value == "a" or value == "A":
         value = "affitto"
     if value == "v" or value == "V":
@@ -117,43 +117,61 @@ def affitto_vendita_callback(value: str) -> str:
 def tipo_immobile_callback(value: str) -> str:
     if value == "c" or value == "C":
         value = "case"
+    if value == "g" or value == "G":
+        value = "garage"
+    if value == "p" or value == "P":
+        value = "palazzi"
+    if value == "u" or value == "U":
+        value = "uffici"
     if value == "n" or value == "N":
         value = "negozi"
-    if value != "case" and value != "negozi":
-        raise typer.BadParameter("Solo 'case' o 'negozi' sono ammessi")
+    if value == "m" or value == "M":
+        value = "magazzini"
+    if value == "cp" or value == "CP":
+        value = "capannoni"
+    if value != "case" and value != "garage" and value != "palazzi" and value != "uffici" and value != "negozi" and value != "magazzini" and value != "capannoni":
+        raise typer.BadParameter("Solo 'case', 'garage', 'palazzi', 'uffici', 'negozi', 'magazzini' o 'capannoni' sono ammessi")
     return value
 
-
-def main(
-    citta: str,
-    affitto_vendita: str = typer.Option(
-        ..., "-affitto_vendita", "-av", callback=affitto_vendita_callback
+"""
+citta: str,
+    investimento: str = typer.Option(
+        ..., "-investimento", "-i", callback=investimento_callback
     ),
-    case_negozi: str = typer.Option(
-        ..., "-case_negozi", "-cn", callback=tipo_immobile_callback
+    tipologia: str = typer.Option(
+        ..., "-tipologia", "-t", callback=tipo_immobile_callback
     ),
     verbose: bool = False,
-) -> None:
+    ) -> None:
+"""
+def main(
+    citta: Annotated[str, typer.Argument(help="Opzioni possibili: [nome-città] oppure [nome-citta]-provincia")],
+    investimento: Annotated[str, typer.Option( "-I", "--investimento", help="Opzioni possibili: 'a' per Affitto o 'v' per Vendita", callback=investimento_callback)] = "",
+    tipologia: Annotated[str, typer.Option( "-T", "--tipologia", help="Opzioni possibili: 'c' per Case, 'g' per Garage, 'p' per Palazzi, 'u' per Uffici, 'n' per Negozi, 'm' per Magazzini', 'cp' per Capannoni", callback=tipo_immobile_callback)] = "",
+    verbose: bool = False,
+    ) -> None:
     """
-    Progetto a scopo didattico di web scraping con XPath sul sito immobiliare.it
-    Cerca nella città fornita come argomento, in affitto o in vendita, case o negozi.
-    Output salvato come file Excel.
-    Esempio, per cercare case in vendita a Biella:
+    CLI di scraping del sito immobiliare.it per cercare case o negozi,
+    in vendita o in affitto, di una determinata località.
+    Le opzioni affitto/vendita e case/negozi/magazzini sono obbligatorie, così come 
+    ovviamente la località.
     
-    python immobili.py -av v -cn c Biella
+    Esempio di esecuzione per cercare case in vendita a Biella:
+
+    python immobili.py -i v -t c Biella
     """
     # Parsing delle pagine; risultati nella var globale lista_immobili
     # Costruzione url, richiamo parser e costruzione dataframe
     # Gestione città formate da più parole, es Casale Monferrato
     citta_url = citta.replace(" ", "-").lower()
     global lista_immobili
-    url_padre = f"{URL_SITO}{affitto_vendita.lower()}-{case_negozi.lower()}/{citta_url}/?criterio=rilevanza"
+    url_padre = f"{URL_SITO}{investimento.lower()}-{tipologia.lower()}/{citta_url}/?criterio=rilevanza"
     lista_pagine = get_urls(url_padre)
     lista_immobili = []
     numero_pagine = len(lista_pagine)
     with typer.progressbar(
         enumerate(lista_pagine),
-        label=f"Sto cercando {case_negozi} in {affitto_vendita} a {citta.capitalize()}...",
+        label=f"Sto cercando {tipologia} in {investimento} a {citta.capitalize()}...",
         length=numero_pagine,
     ) as progress:
         for indx, i in progress:
@@ -177,9 +195,9 @@ def main(
     else:
         citta_nomefile = citta
     df.to_excel(
-        f"{affitto_vendita[0].capitalize()}-{case_negozi[0].capitalize()}-{citta_nomefile.capitalize()}-{today.strftime('%d-%m-%y')}.xlsx",
+        f"{investimento[0].capitalize()}-{tipologia[0].capitalize()}-{citta_nomefile.capitalize()}-{today.strftime('%d-%m-%y')}.xlsx",
         index=False,
-        sheet_name=f"{affitto_vendita} {case_negozi} a {citta_nomefile.capitalize()}",
+        sheet_name=f"{investimento} {tipologia} a {citta_nomefile.capitalize()}",
     )
 
 
